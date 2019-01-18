a86c17384057d5cb57a40328e5ae6f9d434ce9f5
wangonya <kwangonya@gmail.com>
2019-01-18 22:15:14 +0300

    add testdb and devdb connection fixtures in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import os
 import pytest
+import psycopg2
+
+from psycopg2.extras import RealDictCursor
 
 import app
 
@@ -6,5 +10,35 @@ import app
 @pytest.fixture
 def main():
     """setup testing module with testing config"""
-    test = app.create_app("dev")
-    return test.test_client()
+    test = app.create_app("testing")
+
+    with test.app_context():
+        yield test.test_client()
+
+
+@pytest.fixture
+def testdb_cnxn(main):
+    testdb_cnxn = psycopg2.connect(os.getenv("TESTING_DB_URI"))
+    yield testdb_cnxn
+    testdb_cnxn.close()
+
+
+@pytest.fixture
+def testdb_cursor(testdb_cnxn):
+    testdb_cursor = testdb_cnxn.cursor(cursor_factory=RealDictCursor)
+    yield testdb_cursor
+    testdb_cnxn.rollback()
+
+
+@pytest.fixture
+def devdb_cnxn(main):
+    devdb_cnxn = psycopg2.connect(os.getenv("DEV_DB_URI"))
+    yield devdb_cnxn
+    devdb_cnxn.close()
+
+
+@pytest.fixture
+def devdb_cursor(devdb_cnxn):
+    devdb_cursor = devdb_cnxn.cursor(cursor_factory=RealDictCursor)
+    yield devdb_cursor
+    devdb_cnxn.rollback()
