efc1f39ad265ba18fadc9004514afe05fddf65fe
wangonya <kwangonya@gmail.com>
2019-01-20 09:26:20 +0300

    [starts #163298995] add email format and duplicate user error handlers

--- app/utils/error_handlers.py
+++ app/utils/error_handlers.py
@@ -8,6 +8,18 @@ class DatabaseConnectionError(HTTPException):
 
 
 class TableCreationError(HTTPException):
-    """handle database connection error"""
+    """handle table creation error"""
     code = 500
     description = "An error occurred while creating the tables"
+
+
+class InvalidEmailFormatError(HTTPException):
+    """handle invalid email format"""
+    code = 400
+    description = "Invalid email format"
+
+
+class UserAlreadyExistsError(HTTPException):
+    """handle duplicate user registration"""
+    code = 409
+    description = "A user with that email already exists"
