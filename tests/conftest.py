9fa32c574c7d12fb9f769c7a241f12dcd91e446c
wangonya <kwangonya@gmail.com>
2019-01-21 21:46:02 +0300

    add more fixtures and helper functions to reuse in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import os
+import json
 import pytest
 import psycopg2
 
 from psycopg2.extras import RealDictCursor
 
 import app
+from app.auth.models import AuthModel
 
 
 @pytest.fixture
@@ -20,6 +22,7 @@ def main():
 def cnxn(main):
     """testdb connection fixture"""
     cnxn = psycopg2.connect(os.getenv("TESTING_DB_URI"))
+    cnxn.autocommit = True
     yield cnxn
     cnxn.close()
 
@@ -30,3 +33,33 @@ def cursor(cnxn):
     cursor = cnxn.cursor(cursor_factory=RealDictCursor)
     yield cursor
     cnxn.rollback()
+
+
+@pytest.fixture
+def dev_cnxn(main):
+    """testdb connection fixture"""
+    dev_cnxn = psycopg2.connect(os.getenv("DEV_DB_URI"))
+    dev_cnxn.autocommit = True
+    yield dev_cnxn
+    dev_cnxn.close()
+
+
+@pytest.fixture
+def dev_cursor(dev_cnxn):
+    """testdb cursor fixture"""
+    dev_cursor = dev_cnxn.cursor(cursor_factory=RealDictCursor)
+    yield dev_cursor
+    dev_cnxn.rollback()
+
+
+@pytest.fixture
+def new_user():
+    """reuse this in auth test to test AuthModel user registration"""
+    user = AuthModel('fname', 'lname', 'test@gmail.com', '23432432', 'test_pass!')
+    yield user
+
+
+# helper functions dealing with data
+def post_json(main, url, json_dict):
+    """send a json dict to the specified url """
+    return main.post(url, data=json.dumps(json_dict), content_type='application/json')
