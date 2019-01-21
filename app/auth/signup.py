92ab3e5531b109632278f1c03581e6c57d165839
wangonya <kwangonya@gmail.com>
2019-01-21 21:53:30 +0300

    [finishes #163298995] add code for signup endpoint

---
+++ app/auth/signup.py
@@ -0,0 +1,54 @@
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource, reqparse
+
+from ..utils.validators import AuthValidators
+from ..auth.models import AuthModel
+
+
+class Signup(Resource):
+    """signup endpoint resource"""
+    parser = reqparse.RequestParser()
+    parser.add_argument("firstname",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!")
+    parser.add_argument("lastname",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!")
+    parser.add_argument("email",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!")
+    parser.add_argument("password",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!")
+    parser.add_argument("phonenumber",
+                        type=str)
+
+    @staticmethod
+    def post():
+        """do a POST to signup endpoint"""
+        data = Signup.parser.parse_args()
+
+        # check that email is in a correct format
+        AuthValidators.check_email_format(data["email"])
+
+        # check if email already exists to avoid duplicates
+        AuthValidators.check_email_exists(data["email"])
+
+        # check that password has appropriate length
+        AuthValidators.check_password_length(data["password"])
+
+        user = AuthModel(**data)
+        user.save_to_db()
+
+        response = {
+            "status": 201,
+            "message": "user registered successfully",
+            "data": [{
+                "access_token": create_access_token(data["email"])
+                }]}
+
+        return response, 201
