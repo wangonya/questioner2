9ca58e6b6e2ee619dc104119724ee6260a394f4e
wangonya <kwangonya@gmail.com>
2019-01-22 06:57:06 +0300

    remove comments

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -13,7 +13,6 @@ def test_sign_up_valid(new_user, cursor, main):
                    (new_user.firstname, new_user.lastname, new_user.email,
                     new_user.phonenumber, new_user.password, new_user.username))
 
-    # check that values passed in are the ones in the db
     cursor.execute('SELECT * FROM users;')
     data = cursor.fetchone()
     assert isinstance(data["id"], int)
@@ -25,7 +24,6 @@ def test_sign_up_valid(new_user, cursor, main):
     assert new_user.username[0] == data["username"] == "test"
     assert isinstance(data["registered"], datetime.date)
 
-    # clear table after tests
     cursor.execute('TRUNCATE TABLE users;')
 
     res = post_json(main, "/api/v2/auth/signup", new_user.__dict__)
