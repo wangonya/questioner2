b76bd71ae168849bf3c2b5ee6348b28a3e05ebe5
wangonya <kwangonya@gmail.com>
2019-01-24 06:36:26 +0300

    [Finishes #163299922] Add code for rsvps endpoint

---
+++ app/meetups/rsvps.py
@@ -0,0 +1,49 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from ..meetups.models import MeetupModel, RsvpsModel
+from ..auth.models import AuthModel
+from ..utils.validators import RsvpValidators, MeetupValidators
+
+
+class Rsvp(Resource):
+    """resource for Rsvp endpoint"""
+    parser = reqparse.RequestParser()
+    parser.add_argument("status",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!")
+
+    @jwt_required
+    def post(self, m_id):
+        """do POST on rsvp endpoint"""
+        data = Rsvp.parser.parse_args()
+        status = data["status"]
+        user = get_jwt_identity()
+        userid = AuthModel.find_by_email(user)
+
+        MeetupValidators.check_meetup_exists(m_id)
+        RsvpValidators.check_proper_rsvp(status)
+
+        rsvp = RsvpsModel(status, userid, m_id)
+        if not RsvpsModel.check_duplicate_rsvp(userid, m_id):
+            rsvp.save_rsvp_to_db()
+            response = {
+                "status": 201,
+                "msg": "meetup rsvp successful",
+                "data": [{
+                    "m_id": m_id,
+                    "status": status
+                }]
+            }
+            return response, 201
+        else:
+            RsvpsModel.update_rsvp(status, userid, m_id)
+            response = {
+                "status": 200,
+                "msg": "meetup rsvp update successful",
+                "data": [{
+                    "m_id": m_id,
+                    "status": status
+                }]
+            }
+            return response, 200
