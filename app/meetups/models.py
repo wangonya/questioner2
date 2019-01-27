13853498a891f3077f985fee2f28527ba9e38095
wangonya <kwangonya@gmail.com>
2019-01-27 10:40:47 +0300

    [Bug #163498903] Remove duplication

--- app/meetups/models.py
+++ app/meetups/models.py
@@ -1,15 +1,9 @@
-import os
-
-from psycopg2.extras import RealDictCursor
-
-from ..utils.validators import DbValidators
+from ..db import InitDb
+from ..db.select import SelectDataFromDb
 
 
 class MeetupModel:
     """model to handle meetup data"""
-    cnxn = DbValidators.connect_to_db(os.getenv("DEV_DB_URI"))
-    cnxn.autocommit = True
-    cursor = cnxn.cursor(cursor_factory=RealDictCursor)
 
     def __init__(self, title, creator, location,
                  happening_on, tags, image):
@@ -26,41 +20,27 @@ class MeetupModel:
                         '(title, creator, location, '
                         'happening_on, tags, image) '
                         'VALUES (%s, %s, %s, %s, %s, %s);')
-        MeetupModel.cursor.execute(insert_query,
-                                   (self.title, self.creator, self.location,
-                                    self.happening_on, self.tags, self.image))
+        InitDb.cursor.execute(insert_query,
+                              (self.title, self.creator, self.location,
+                               self.happening_on, self.tags, self.image))
 
-    @classmethod
-    def get_upcoming_meetups(cls):
+    @staticmethod
+    def get_upcoming_meetups():
         """get all upcoming meetups"""
-        cls.cursor.execute('SELECT * '
-                           'FROM meetups')
-        meetups = cls.cursor.fetchall()
+        meetups = SelectDataFromDb.general_select("meetups")
         return meetups
 
-    @classmethod
-    def get_specific_meetup(cls, m_id):
+    @staticmethod
+    def get_specific_meetup(m_id):
         """get all upcoming meetups"""
-        cls.cursor.execute('SELECT * '
-                           'FROM meetups '
-                           'WHERE id = (%s)', (m_id,))
-        meetups = cls.cursor.fetchall()
+        meetups = SelectDataFromDb.conditional_where_select("meetups", "id", m_id)
         return meetups
 
-    @classmethod
-    def find_meetup(cls, title):
-        """check if a meetup with the same title already exists"""
-        cls.cursor.execute('SELECT * '
-                           'FROM meetups '
-                           'WHERE title = (%s)', (title,))
-        meetup = cls.cursor.fetchone()
-        return meetup
-
-    @classmethod
-    def delete_meetup(cls, m_id):
+    @staticmethod
+    def delete_meetup(m_id):
         """delete meetup"""
-        cls.cursor.execute('DELETE FROM meetups '
-                           'WHERE id = (%s)', (m_id,))
+        InitDb.cursor.execute('DELETE FROM meetups '
+                              'WHERE id = (%s)', (m_id,))
 
 
 class RsvpsModel:
@@ -76,24 +56,21 @@ class RsvpsModel:
         insert_query = ('INSERT INTO rsvps '
                         '(status, creator, meetup) '
                         'VALUES (%s, %s, %s);')
-        MeetupModel.cursor.execute(insert_query,
-                                   (self.status, self.uid["id"], self.m_id))
+        InitDb.cursor.execute(insert_query,
+                              (self.status.lower(), self.uid["id"], self.m_id))
 
-    @classmethod
-    def check_duplicate_rsvp(cls, user, m_id):
+    @staticmethod
+    def check_duplicate_rsvp(user, m_id):
         """check if user has already rsvp'd
         if they have, updte the record"""
-        MeetupModel.cursor.execute('SELECT * '
-                                   'FROM rsvps '
-                                   'WHERE meetup = {} '
-                                   'AND creator = {}'.format(m_id, user["id"]))
-        rsvp = MeetupModel.cursor.fetchone()
+        data = ["meetup", m_id, "creator", user["id"]]
+        rsvp = SelectDataFromDb.conditional_where_and_select("rsvps", *data)
         return rsvp
 
     @staticmethod
     def update_rsvp(status, user, meetup):
         """update rsvp"""
-        delete_query = ('UPDATE rsvps SET status = %s '
-                        'WHERE creator = %s '
-                        'AND meetup = %s')
-        MeetupModel.cursor.execute(delete_query, (status, user["id"], meetup))
+        query = ('UPDATE rsvps SET status = %s '
+                 'WHERE creator = %s '
+                 'AND meetup = %s')
+        InitDb.cursor.execute(query, (status.lower(), user["id"], meetup))
