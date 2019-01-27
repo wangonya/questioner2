a08041234e4b2ebda5e1f4b24ab911a2e67b9648
wangonya <kwangonya@gmail.com>
2019-01-27 10:40:24 +0300

    [Bug #163498903] Use new select methods to select data

--- app/meetups/delete.py
+++ app/meetups/delete.py
@@ -2,9 +2,9 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ..meetups.models import MeetupModel
-from ..auth.models import AuthModel
 from ..utils.error_handlers import DeleteProtectedError
 from ..utils.validators import MeetupValidators
+from ..db.select import SelectDataFromDb
 
 
 class DeleteMeetup(Resource):
@@ -13,12 +13,12 @@ class DeleteMeetup(Resource):
     @jwt_required
     def delete(m_id):
         user = get_jwt_identity()
-        userid = AuthModel.find_by_email(user)
+        userid = SelectDataFromDb.conditional_where_select("users", "email", user)
         meetup = MeetupModel.get_specific_meetup(m_id)
 
         MeetupValidators.check_meetup_exists(m_id)
 
-        if not userid["id"] == meetup[0]["creator"]:
+        if not userid["id"] == meetup["creator"]:
             raise DeleteProtectedError
         else:
             MeetupModel.delete_meetup(m_id)
